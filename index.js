818c9eb4abdbc46c02a79a648f661d4c96041622
Nitesh Kumar <79629635+Nitesh600@users.noreply.github.com>
2025-01-24 15:00:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.listen(PORT, () => {
 });
 app.use(
   cors({
-    origin: ["https://auth-client-nine.vercel.app/signup"],
+    origin: ["https://auth-client-nine.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
